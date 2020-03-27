6bb0af8c96f5bb6ccb77a5aa22eab3a3a13b10a6
onteriowright <onteriowright@yahoo.com>
2020-03-27 10:06:47 -0500

    added walker controller

---
+++ DogWalkerAPI/Controllers/WalkerController.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogWalkerAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DogWalkerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalkerController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public WalkerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+
+        // Get all departments from the database
+        [HttpGet]
+        public async Task<IActionResult> GET()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Name, NeighborhoodId
+                        FROM Walker";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    var walker = new List<Walker>();
+
+                    while (reader.Read())
+                    {
+                        var newWalker = new Walker
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        };
+
+                        walker.Add(newWalker);
+                    }
+                    reader.Close();
+
+                    return Ok(walker);
+                }
+            }
+        }
+
+
+        // Get a single department by Id from database
+        [HttpGet("{id}", Name = "GetWalker")]
+        public async Task<IActionResult> GET([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Name, NeighborhoodId
+                        FROM Walker
+                        WHERE Id = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Owner owner = null;
+
+                    if (reader.Read())
+                    {
+                        owner = new Owner
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                        };
+                        reader.Close();
+
+                        return Ok(owner);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+        }
+
+
+        // Create department and add it to database
+        [HttpPost]
+        public async Task<IActionResult> POST([FromBody] Walker walker)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Walker (Name, NeighborhoodId)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@name, @neighborhoodId)";
+                    cmd.Parameters.Add(new SqlParameter("@name", walker.Name));
+                    cmd.Parameters.Add(new SqlParameter("@neighborhoodId", walker.NeighborhoodId));
+
+                    int newId = (int)cmd.ExecuteScalar();
+                    walker.Id = newId;
+                    return CreatedAtRoute("GetWalker", new { id = newId }, walker);
+                }
+            }
+        }
+
+        //Update single department by id in database
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PUT([FromRoute] int id, [FromBody] Walker walker)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Walker
+                                            SET 
+                                            Name = @name,
+                                            NeighborhoodId = @neighborhoodId
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@name", walker.Name));
+                        cmd.Parameters.Add(new SqlParameter("@neighborhoodId", walker.NeighborhoodId));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        // Delete single department by id from database
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DELETE([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE 
+                                            FROM Walker 
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+
+        // Check to see if department exist by id in database
+        private bool DepartmentExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Name, NeighborhoodId
+                        FROM Walker
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
